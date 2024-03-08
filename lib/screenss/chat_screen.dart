c14763383c55e47e1305b77e0fb3c68685abaa09
wangguiye <wangguiye@lenovo.com>
2024-03-08 21:45:34 +0800

    添加进度条

--- lib/screenss/chat_screen.dart
+++ lib/screenss/chat_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +16,9 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen>{
   final _auth = FirebaseAuth.instance;
+  final _firebaseStore = FirebaseFirestore.instance;
   late User loggedInUser;
+  late String messageText;
 
   @override
   void initState() {
@@ -32,6 +35,22 @@ class _ChatScreenState extends State<ChatScreen>{
       }
     }catch(e){
       print(e);
+    }
+  }
+
+  void getMessages() async {
+    // Future<QuerySnapshot<T>>
+    final messages = await _firebaseStore.collection('messages').get();
+    for(var message in messages.docs){
+      print(message.data());
+    }
+  }
+  
+  void getStreamMessages() async {
+    await for(var snapshot in _firebaseStore.collection('messages').snapshots()){
+      for(var message in snapshot.docs){
+        print(message.data());
+      }
     }
   }
 
@@ -56,26 +75,69 @@ class _ChatScreenState extends State<ChatScreen>{
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            // StreamBuilder<QuerySnapshot>(
+            //     stream: _firebaseStore.collection('messages').snapshots(),
+                // builder: (BuildContext context, AsyncSnapshot<String> snapshot){
+                //   final messages = snapshot.data.docs;
+                //   // List<Text> messageWidgets = [];
+                //   // for(var message in messages){
+                //   //   final messageText = message.data('msg');
+                //   //   final messageSender = message.data();
+                //   //
+                //   //   final messageWidget = Text('$messageText from $');
+                //   // }
+                //   return Text(''),
+                // },
+
+            // ),
             Container(
               // decoration: ,
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Expanded(child: TextField(
-                    onChanged: (value){},
+                    onChanged: (value){
+                      messageText = value;
+                    },
                     // decoration: ,
                   )),
                   // FlatButton(
                   // );
 
                   TextButton(
-                      onPressed: (){},
+                      onPressed: (){
+                        _firebaseStore.collection('messages').add({
+                          'msg':messageText,
+                          'sender': loggedInUser.email,
+                        });
+                      },
                       child: Text(
                         'Send',
                         // style: ,
                       ),
                   ),
 
+
+                  TextButton(
+                    onPressed: ()  {
+                      getMessages();
+                    },
+                    child: Text(
+                      'Get',
+                      // style: ,
+                    ),
+                  ),
+
+
+                  TextButton(
+                    onPressed: ()  {
+                      getStreamMessages();
+                    },
+                    child: Text(
+                      'Stream Get',
+                      // style: ,
+                    ),
+                  ),
                 ],
               ),
             ),
